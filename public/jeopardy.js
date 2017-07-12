d3d10a244311ca9e6f6bd5fae7aa19cc19a23160
jchurley95 <jchurley95@gmail.com>
2017-07-12 12:06:43 -0400

    added more pseudocode, created jeoColumn object containing string for category, 100, 200, 300, 400, 500

---
+++ public/jeopardy.js
@@ -0,0 +1,38 @@
+
+//master const object
+
+
+//APPENDING A GAMEBOARD COLUMN AS ITS OWN DIV
+//Append a div with Category Name
+//Append 100 point div
+    //Clicking on this div appends 100 point question to answer_board
+//Append 200 point div
+    //Clicking on this div appends 200 point question to answer_board
+//Append 300 point div
+    //Clicking on this div appends 300 point question to answer_board
+//Append 400 point div
+    //Clicking on this div appends 400 point question to answer_board
+//Append 500 point div
+    //Clicking on this div appends 500 point question to answer_board
+
+
+//SELECTING A QUESTION
+//GIVEN I am on the gameboard page 
+//WHEN I click on a question 
+//THEN takes me to the answer question page
+
+
+//SUBMITTING CORRECT ANSWER
+//GIVEN I am on the page for answering a question 
+//WHEN I submit my answer 
+//AND answer is correct 
+//THEN Tells me answer is correct 
+//AND adds to my score
+
+
+//SUBMITTING INCORRECT ANSWER
+//GIVEN I am on the page for answering a question 
+//WHEN I submit my answer 
+//AND answer is incorrect 
+//THEN Tells me answer is incorrect 
+//AND subtracts points from my score
