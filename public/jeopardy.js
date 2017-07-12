f7c4c21b7a710d59a224698aae631e59efcb97ed
jchurley95 <jchurley95@gmail.com>
2017-07-12 15:42:37 -0400

    Wrapped a function around the for loop called displayColumn, added another function called showQuestion which appends currently just a string saying the word question to my question_board div. Also, changed the for loop so that it gives each button an id of categoryID plus i and an onclick function which calls showQuestion

--- public/jeopardy.js
+++ public/jeopardy.js
@@ -5,8 +5,26 @@
 //APPENDING A GAMEBOARD COLUMN AS ITS OWN DIV
 
 //Loop (through an object containing these)
-var categoryName = "Example Category Name";
+var categoryName = "United States History";
+var categoryId = "us_history";
 var jeoColumn = [categoryName, 100, 200, 300, 400, 500];
+/*var questionAndAnswer = {
+    ["United States History",
+    ["Year the United States gained its independence from the British Empire", "What is 1776?"],
+    ["First African American President of the United States", "Who is Barrack Obama?"],
+    ["Treaty that ended the Revolutionary War", "What is the Treaty of Paris?"],
+    ["President during the 19th and 20th century", "Who was William McKinley?"],
+    ["This was the last US Navy ship sunk as a result of WW2", "What was the USS Bullhead?"]
+    ],
+    ["United States History",
+    ["Year the United States gained its independence from the British Empire", "What is 1776?"],
+    ["First African American President of the United States", "Who is Barrack Obama?"],
+    ["Treaty that ended the Revolutionary War", "What is the Treaty of Paris?"],
+    ["President during the 19th and 20th century", "Who was William McKinley?"],
+    ["This was the last US Navy ship sunk as a result of WW2", "What was the USS Bullhead?"]
+    ]
+};
+*/
 
 //Append a div with Category Name
     //Append a 100 point div
@@ -20,21 +38,36 @@ var jeoColumn = [categoryName, 100, 200, 300, 400, 500];
     //Append a 500 point div
         //Clicking on this div appends 500 point question to answer_board
 
+var displayColumn= function(categoryName, categoryId) {
+    var pointsWorth = 100;
     for(var i = 0; i < jeoColumn.length; i++){
         if(i === 0) {
-            $('#column1').append('<div class="category"> '+ jeoColumn[i] + '</div><br>');
+            $('#column1').append('<div class="category"> '+ categoryName + '</div><br>');
         } else {
-            $('#column1').append('<button class="game_block"> '+ jeoColumn[i] + '</button><br>');
+            $('#column1').append('<button class="game_block" id="' + categoryId + '-' + i + '" onclick="showQuestion()">'+ pointsWorth + '</button><br>');
+            pointsWorth +=100;
         }
     }
+}
+
+displayColumn('United States History', 'us_history');
 
 //SELECTING A QUESTION
 //GIVEN I am on the gameboard page 
-//WHEN I click on a question 
+//WHEN I click on a game block 
 //THEN makes question appear in footer
-//showQuestion: function() {
 
-//}
+var showQuestion = function() {
+   $('#question_board').append("question");
+
+}
+
+var removeQuestion = function() {
+   $('#question_board').remove("question");
+
+}
+
+
 
 //SUBMITTING CORRECT ANSWER
 //GIVEN I am on the page for answering a question 
